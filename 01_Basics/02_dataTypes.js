c62582e50732be41ca03467c720400eda502b46b
Avadhut Jadhav <95961586+avadhutjadhav@users.noreply.github.com>
2024-05-08 19:14:02 +0000

    DataType

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,9 @@
+"use strict"; //treat all JS code as newer version
+
+//alert (3+3); //We are using NodeJs not Browsers
+
+//Datatypes : Number, string, Boolean, Null, undefined, Object, Symbol
+
+console.log( typeof "Avadhut");
+console.log(typeof undefined);
+console.log(typeof null);
